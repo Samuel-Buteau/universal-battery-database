eb05b58e06d33fc67d8f2be6d5945fbc2c2d002c
Samuel Buteau <Samuel.Buteau@dal.ca>
2019-10-21 18:51:18 -0300

    the deep clean is started. Structured the electrodes. separator still messy

--- ElectrolyteDatabase/read_electrolyte_csv.py
+++ ElectrolyteDatabase/read_electrolyte_csv.py
@@ -1,7 +1,7 @@
 
 def electrolyte_csv_to_db(file):
 
-    from ElectrolyteDatabase.models import Electrolyte, Component, Alias, Molecule
+    from ElectrolyteDatabase.models import Electrolyte, ElectrolyteComponent, Alias, ElectrolyteMolecule
     from pandas import DataFrame
     import pandas as pd
     import numpy
@@ -32,153 +32,153 @@ def electrolyte_csv_to_db(file):
 
             if not(not isinstance(row['LiPF6 concentration /molal'],str) and math.isnan(row['LiPF6 concentration /molal'])):
 
-                if len(Molecule.objects.filter(name='LiPF6')) == 0:
-                    Molecule.objects.create(name='LiPF6', can_be_salt=True)
+                if len(ElectrolyteMolecule.objects.filter(name='LiPF6')) == 0:
+                    ElectrolyteMolecule.objects.create(name='LiPF6', can_be_salt=True)
 
                 if re.match(r'\d',str(row['LiPF6 concentration /molal'])) and float(row['LiPF6 concentration /molal']) != 0:
 
-                    e.component_set.create(molecule=Molecule.objects.get(name='LiPF6'), molal=float(row['LiPF6 concentration /molal']))
+                    e.component_set.create(molecule=ElectrolyteMolecule.objects.get(name='LiPF6'), molal=float(row['LiPF6 concentration /molal']))
 
 
             if not(not isinstance(row['Salt NO.2 name'],str) and math.isnan(row['Salt NO.2 name'])):
 
-                if len(Molecule.objects.filter(name=row['Salt NO.2 name'])) == 0:
-                    Molecule.objects.create(name=row['Salt NO.2 name'], can_be_salt=True)
+                if len(ElectrolyteMolecule.objects.filter(name=row['Salt NO.2 name'])) == 0:
+                    ElectrolyteMolecule.objects.create(name=row['Salt NO.2 name'], can_be_salt=True)
 
                 if not(not isinstance(row['Salt NO.2 concentration /molal'],str) and math.isnan(row['Salt NO.2 concentration /molal'])):
                     if re.match(r'\d',str(row['Salt NO.2 concentration /molal'])) and float(row['Salt NO.2 concentration /molal']) != 0:
-                        e.component_set.create(molecule=Molecule.objects.get(name=row['Salt NO.2 name']),
-                                                                             molal=float(row['Salt NO.2 concentration /molal']))
+                        e.component_set.create(molecule=ElectrolyteMolecule.objects.get(name=row['Salt NO.2 name']),
+                                               molal=float(row['Salt NO.2 concentration /molal']))
                 else:
-                    e.component_set.create(molecule=Molecule.objects.get(name=row['Salt NO.2 name']),
-                                                                             molal=1.0)
+                    e.component_set.create(molecule=ElectrolyteMolecule.objects.get(name=row['Salt NO.2 name']),
+                                           molal=1.0)
 
             if not (not isinstance(row['Salt NO.3 name'], str) and math.isnan(row['Salt NO.3 name'])):
 
-                if len(Molecule.objects.filter(name=row['Salt NO.3 name'])) == 0:
-                    Molecule.objects.create(name=row['Salt NO.3 name'], can_be_salt=True)
+                if len(ElectrolyteMolecule.objects.filter(name=row['Salt NO.3 name'])) == 0:
+                    ElectrolyteMolecule.objects.create(name=row['Salt NO.3 name'], can_be_salt=True)
 
                 if not(not isinstance(row['Salt NO.3 concentration /molal'],str) and math.isnan(row['Salt NO.3 concentration /molal'])):
                     if re.match(r'\d',str(row['Salt NO.3 concentration /molal'])) and float(row['Salt NO.3 /molal']) != 0:
-                        e.component_set.create(molecule=Molecule.objects.get(name=row['Salt NO.3 name']),
-                                                                         molal=float(row['Salt NO.3 concentration /molal']))
+                        e.component_set.create(molecule=ElectrolyteMolecule.objects.get(name=row['Salt NO.3 name']),
+                                               molal=float(row['Salt NO.3 concentration /molal']))
                 else:
-                    e.component_set.create(molecule=Molecule.objects.get(name=row['Salt NO.3 name']),
-                                                                         molal=1.0)
+                    e.component_set.create(molecule=ElectrolyteMolecule.objects.get(name=row['Salt NO.3 name']),
+                                           molal=1.0)
 
 
             if not(not isinstance(row['EC /wt%'],str) and math.isnan(row['EC /wt%'])):
 
-                if len(Molecule.objects.filter(name='EC')) == 0:
-                    Molecule.objects.create(name='EC', can_be_additive=True, can_be_solvent=True)
+                if len(ElectrolyteMolecule.objects.filter(name='EC')) == 0:
+                    ElectrolyteMolecule.objects.create(name='EC', can_be_additive=True, can_be_solvent=True)
 
                 if re.match(r'\d',str(row['EC /wt%'])) and float(row['EC /wt%']) != 0:
-                    e.component_set.create(molecule=Molecule.objects.get(name='EC'),
-                                                                         weight_percent=float(row['EC /wt%']))
+                    e.component_set.create(molecule=ElectrolyteMolecule.objects.get(name='EC'),
+                                           weight_percent=float(row['EC /wt%']))
 
             if not(not isinstance(row['EMC /wt%'],str) and math.isnan(row['EMC /wt%'])):
 
-                if len(Molecule.objects.filter(name='EMC')) == 0:
-                    Molecule.objects.create(name='EMC', can_be_additive=True, can_be_solvent=True)
+                if len(ElectrolyteMolecule.objects.filter(name='EMC')) == 0:
+                    ElectrolyteMolecule.objects.create(name='EMC', can_be_additive=True, can_be_solvent=True)
                 if re.match(r'\d',str(row['EMC /wt%'])) and float(row['EMC /wt%']) != 0:
-                    e.component_set.create(molecule=Molecule.objects.get(name='EMC'),
-                                                                         weight_percent=float(row['EMC /wt%']))
+                    e.component_set.create(molecule=ElectrolyteMolecule.objects.get(name='EMC'),
+                                           weight_percent=float(row['EMC /wt%']))
 
             if not(not isinstance(row['DMC /wt%'],str) and math.isnan(row['DMC /wt%'])):
 
-                if len(Molecule.objects.filter(name='DMC')) == 0:
-                    Molecule.objects.create(name='DMC', can_be_additive=True, can_be_solvent=True)
+                if len(ElectrolyteMolecule.objects.filter(name='DMC')) == 0:
+                    ElectrolyteMolecule.objects.create(name='DMC', can_be_additive=True, can_be_solvent=True)
 
                 if re.match(r'\d',str(row['DMC /wt%'])) and float(row['DMC /wt%']) != 0:
-                    e.component_set.create(molecule=Molecule.objects.get(name='DMC'),
-                                                                         weight_percent=float(row['DMC /wt%']))
+                    e.component_set.create(molecule=ElectrolyteMolecule.objects.get(name='DMC'),
+                                           weight_percent=float(row['DMC /wt%']))
 
             if not(not isinstance(row['MA /wt%'],str) and math.isnan(row['MA /wt%'])):
 
-                if len(Molecule.objects.filter(name='MA')) == 0:
-                    Molecule.objects.create(name='MA', can_be_additive=True, can_be_solvent=True)
+                if len(ElectrolyteMolecule.objects.filter(name='MA')) == 0:
+                    ElectrolyteMolecule.objects.create(name='MA', can_be_additive=True, can_be_solvent=True)
 
 
                 if re.match(r'\d',str(row['MA /wt%'])) and float(row['MA /wt%']) != 0:
-                    e.component_set.create(molecule=Molecule.objects.get(name='MA'),
-                                                                         weight_percent=float(row['MA /wt%']))
+                    e.component_set.create(molecule=ElectrolyteMolecule.objects.get(name='MA'),
+                                           weight_percent=float(row['MA /wt%']))
 
 
             if (not(not isinstance(row['Compound No.1 name'],str) and math.isnan(row['Compound No.1 name']))) and (not(not isinstance(row['Compound No.1 /wt%'],str) and math.isnan(row['Compound No.1 /wt%']))):
 
-                if len(Molecule.objects.filter(name=row['Compound No.1 name'])) == 0:
-                    Molecule.objects.create(name=row['Compound No.1 name'], can_be_additive=True, can_be_solvent=True)
+                if len(ElectrolyteMolecule.objects.filter(name=row['Compound No.1 name'])) == 0:
+                    ElectrolyteMolecule.objects.create(name=row['Compound No.1 name'], can_be_additive=True, can_be_solvent=True)
 
 
                 if re.match(r'\d',str(row['Compound No.1 /wt%'])) and float(row['Compound No.1 /wt%']) != 0:
-                    e.component_set.create(molecule=Molecule.objects.get(name=row['Compound No.1 name']),
-                                                                         weight_percent=float(row['Compound No.1 /wt%']))
+                    e.component_set.create(molecule=ElectrolyteMolecule.objects.get(name=row['Compound No.1 name']),
+                                           weight_percent=float(row['Compound No.1 /wt%']))
 
                 else:
-                    e.component_set.create(molecule=Molecule.objects.get(name=row['Compound No.1 name']))
+                    e.component_set.create(molecule=ElectrolyteMolecule.objects.get(name=row['Compound No.1 name']))
 
             if (not (not isinstance(row['Compound No.2 name2'], str) and math.isnan(row['Compound No.2 name2']))) and (
             not (not isinstance(row['Compound No.2 /wt%'], str) and math.isnan(
                     row['Compound No.2 /wt%']))):
 
-                if len(Molecule.objects.filter(name=row['Compound No.2 name2'])) == 0:
-                    Molecule.objects.create(name=row['Compound No.2 name2'], can_be_additive=True, can_be_solvent=True)
+                if len(ElectrolyteMolecule.objects.filter(name=row['Compound No.2 name2'])) == 0:
+                    ElectrolyteMolecule.objects.create(name=row['Compound No.2 name2'], can_be_additive=True, can_be_solvent=True)
 
                 if re.match(r'\d',str(row['Compound No.2 /wt%'])) and float(row['Compound No.2 /wt%']) != 0:
-                    e.component_set.create(molecule=Molecule.objects.get(name=row['Compound No.2 name2']),
+                    e.component_set.create(molecule=ElectrolyteMolecule.objects.get(name=row['Compound No.2 name2']),
                                            weight_percent=float(row['Compound No.2 /wt%']))
 
                 else:
-                    e.component_set.create(molecule=Molecule.objects.get(name=row['Compound No.2 name2']))
+                    e.component_set.create(molecule=ElectrolyteMolecule.objects.get(name=row['Compound No.2 name2']))
 
             if (not(not isinstance(row['Compound No.3 name'],str) and math.isnan(row['Compound No.3 name']))) and (not(not isinstance(row['Compound No.3 /wt%'],str) and math.isnan(row['Compound No.3 /wt%']))):
 
-                if len(Molecule.objects.filter(name=row['Compound No.3 name'])) == 0:
-                    Molecule.objects.create(name=row['Compound No.3 name'], can_be_additive=True, can_be_solvent=True)
+                if len(ElectrolyteMolecule.objects.filter(name=row['Compound No.3 name'])) == 0:
+                    ElectrolyteMolecule.objects.create(name=row['Compound No.3 name'], can_be_additive=True, can_be_solvent=True)
 
 
                 if re.match(r'\d',str(row['Compound No.3 /wt%'])) and float(row['Compound No.3 /wt%']) != 0:
-                    e.component_set.create(molecule=Molecule.objects.get(name=row['Compound No.3 name']),
-                                                                         weight_percent=float(row['Compound No.3 /wt%']))
+                    e.component_set.create(molecule=ElectrolyteMolecule.objects.get(name=row['Compound No.3 name']),
+                                           weight_percent=float(row['Compound No.3 /wt%']))
 
                 else:
-                    e.component_set.create(molecule=Molecule.objects.get(name=row['Compound No.3 name']))
+                    e.component_set.create(molecule=ElectrolyteMolecule.objects.get(name=row['Compound No.3 name']))
             if (not(not isinstance(row['Compound No.4 name'],str) and math.isnan(row['Compound No.4 name']))) and (not(not isinstance(row['Compound No.4 /wt%'],str) and math.isnan(row['Compound No.4 /wt%']))):
 
-                if len(Molecule.objects.filter(name=row['Compound No.4 name'])) == 0:
-                    Molecule.objects.create(name=row['Compound No.4 name'], can_be_additive=True, can_be_solvent=True)
+                if len(ElectrolyteMolecule.objects.filter(name=row['Compound No.4 name'])) == 0:
+                    ElectrolyteMolecule.objects.create(name=row['Compound No.4 name'], can_be_additive=True, can_be_solvent=True)
 
 
                 if re.match(r'\d',str(row['Compound No.4 /wt%'])) and float(row['Compound No.4 /wt%']) != 0:
-                    e.component_set.create(molecule=Molecule.objects.get(name=row['Compound No.4 name']),
-                                                                         weight_percent=float(row['Compound No.4 /wt%']))
+                    e.component_set.create(molecule=ElectrolyteMolecule.objects.get(name=row['Compound No.4 name']),
+                                           weight_percent=float(row['Compound No.4 /wt%']))
 
                 else:
-                    e.component_set.create(molecule=Molecule.objects.get(name=row['Compound No.4 name']))
+                    e.component_set.create(molecule=ElectrolyteMolecule.objects.get(name=row['Compound No.4 name']))
             if (not(not isinstance(row['Compound No.5 name'],str) and math.isnan(row['Compound No.5 name']))) and (not(not isinstance(row['Compound No.5 /wt%'],str) and math.isnan(row['Compound No.5 /wt%']))):
 
-                if len(Molecule.objects.filter(name=row['Compound No.5 name'])) == 0:
-                    Molecule.objects.create(name=row['Compound No.5 name'], can_be_additive=True, can_be_solvent=True)
+                if len(ElectrolyteMolecule.objects.filter(name=row['Compound No.5 name'])) == 0:
+                    ElectrolyteMolecule.objects.create(name=row['Compound No.5 name'], can_be_additive=True, can_be_solvent=True)
 
 
                 if re.match(r'\d',str(row['Compound No.5 /wt%'])) and float(row['Compound No.5 /wt%']) != 0:
-                    e.component_set.create(molecule=Molecule.objects.get(name=row['Compound No.5 name']),
-                                                                         weight_percent=float(row['Compound No.5 /wt%']))
+                    e.component_set.create(molecule=ElectrolyteMolecule.objects.get(name=row['Compound No.5 name']),
+                                           weight_percent=float(row['Compound No.5 /wt%']))
 
                 else:
-                    e.component_set.create(molecule=Molecule.objects.get(name=row['Compound No.5 name']))
+                    e.component_set.create(molecule=ElectrolyteMolecule.objects.get(name=row['Compound No.5 name']))
             if (not(not isinstance(row['Compound No.6 name'],str) and math.isnan(row['Compound No.6 name']))) and (not(not isinstance(row['Compound No.6 /wt%'],str) and math.isnan(row['Compound No.6 /wt%']))):
 
-                if len(Molecule.objects.filter(name=row['Compound No.6 name'])) == 0:
-                    Molecule.objects.create(name=row['Compound No.6 name'], can_be_additive=True, can_be_solvent=True)
+                if len(ElectrolyteMolecule.objects.filter(name=row['Compound No.6 name'])) == 0:
+                    ElectrolyteMolecule.objects.create(name=row['Compound No.6 name'], can_be_additive=True, can_be_solvent=True)
 
 
                 if re.match(r'\d',str(row['Compound No.6 /wt%'])) and float(row['Compound No.6 /wt%']) != 0:
-                    e.component_set.create(molecule=Molecule.objects.get(name=row['Compound No.6 name']),
-                                                                         weight_percent=float(row['Compound No.6 /wt%']))
+                    e.component_set.create(molecule=ElectrolyteMolecule.objects.get(name=row['Compound No.6 name']),
+                                           weight_percent=float(row['Compound No.6 /wt%']))
 
                 else:
-                    e.component_set.create(molecule=Molecule.objects.get(name=row['Compound No.6 name']))
+                    e.component_set.create(molecule=ElectrolyteMolecule.objects.get(name=row['Compound No.6 name']))
 
             print(e.component_set.all())
 

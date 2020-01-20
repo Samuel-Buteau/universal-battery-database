fixed rates derivatives

neware_parser/management/commands/DegradationModel.py
@@ -140,12 +140,17 @@ class DegradationModel(Model):
 
                 res = tf.reshape(nn(params), [-1, 1])
 
+            # NOTE: why do some have `[:, 0, :]` and others don't?
             derivatives['dCyc'] = tape2.batch_jacobian(
                 source=params["cycles"],
                 target=res
             )[:, 0, :]
-            derivatives['dRates'] = tape2.batch_jacobian(
-                source=params["rates"],
+            derivatives['d_chg_rate'] = tape2.batch_jacobian(
+                source=params["chg_rate"],
+                target=res
+            )[:, 0, :]
+            derivatives['d_dchg_rate'] = tape2.batch_jacobian(
+                source=params["dchg_rate"],
                 target=res
             )[:, 0, :]
             derivatives['dFeatures'] = tape2.batch_jacobian(
@@ -158,9 +163,13 @@ class DegradationModel(Model):
             source=params["cycles"],
             target=derivatives['dCyc']
         )[:, 0, :]
-        derivatives['d2Rates'] = tape3.batch_jacobian(
-            source=params["rates"],
-            target=derivatives['dRates']
+        derivatives['d2_chg_rate'] = tape3.batch_jacobian(
+            source=params["chg_rate"],
+            target=derivatives['d_chg_rate']
+        )
+        derivatives['d2_dchg_rate'] = tape3.batch_jacobian(
+            source=params["dchg_rate"],
+            target=derivatives['d_dchg_rate']
         )
         derivatives['d2Features'] = tape3.batch_jacobian(
             source=params["features"],

neware_parser/management/commands/ml_smoothing.py
@@ -507,10 +507,13 @@ def train_step(params, fit_args):
                 + tf.reduce_mean(tf.nn.relu(-eq_vol))
                 # resistance should not decrease.
                 + 10  * tf.reduce_mean(tf.abs(r_der['dCyc']))
-                # equilibrium voltage should not change much
-                + 10. * tf.reduce_mean(tf.abs(eq_vol_der['dCyc']))
-                # equilibrium voltage should not change much
-                + 10. * tf.reduce_mean(tf.abs(eq_vol_der['dRates']))
+                + 10. * (
+                    tf.reduce_mean(tf.abs(eq_vol_der['dCyc']))
+                    # equilibrium voltage should not change much
+                    # TODO is this correct?
+                    + tf.reduce_mean(tf.abs(eq_vol_der["d_chg_rate"]))
+                    + tf.reduce_mean(tf.abs(eq_vol_der["d_dchg_rate"]))
+                )
             )
         )
 
@@ -526,8 +529,9 @@ def train_step(params, fit_args):
             # more ok to accelerate UPWARDS
             + 10. * tf.reduce_mean(tf.square(tf.nn.relu(-r_der['d2Cyc']))
             + 0.5 * tf.square(tf.nn.relu(r_der['d2Cyc'])))
-            + 1.* tf.reduce_mean(tf.square((eq_vol_der['d2Rates'])))
-            + 1.* tf.reduce_mean(tf.square((eq_vol_der['d2Cyc'])))
+            + 1. * tf.reduce_mean(tf.square((eq_vol_der["d_chg_rate"])))
+            + 1. * tf.reduce_mean(tf.square((eq_vol_der["d_dchg_rate"])))
+            + 1. * tf.reduce_mean(tf.square((eq_vol_der['d2Cyc'])))
         )
 
         const_f_loss = fit_args['const_f_coeff'] * (
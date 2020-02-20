65719de7e17f1d9d0268acf2b34b64022ed491c5
Harvey Wang <harvey@dal.ca>
2020-02-20 14:39:33 -0400

    updated smoothing.sh

--- smoothing.sh
+++ smoothing.sh
@@ -2,5 +2,5 @@
 
 mkdir $1
 echo $2 > $1/notes.txt
-python3 manage.py ml_smoothing --path_to_dataset compiled_datasets1 --dataset_version TESTING0 --path_to_plots=$1
+python3 manage.py ml_smoothing --path_to_dataset compiled_datasets1 --dataset_version TESTING1 --path_to_plots=$1
 

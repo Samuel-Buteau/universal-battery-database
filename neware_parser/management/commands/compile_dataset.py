2c54a90d5f541720352dee864432343eb80cca34
Samuel Buteau <Samuel.Buteau@dal.ca>
2020-02-21 09:46:39 -0400

    fixing the importing of step data

--- neware_parser/management/commands/compile_dataset.py
+++ neware_parser/management/commands/compile_dataset.py
@@ -205,26 +205,49 @@ def initial_processing(my_barcodes, fit_args):
         for cyc_group in CycleGroup.objects.filter(
                 barcode=barcode
         ).order_by('discharging_rate'):
-            result = []
+            result_dchg = []
+            result_chg = []
+
             for cyc in cyc_group.cycle_set.order_by(
                     'cycle_number'):
                 if cyc.valid_cycle:
-                    vq_curve, vq_mask, step_info = machine_learning_post_process_cycle(cyc, voltage_grid)
-                    if vq_curve is None:
+                    vq_curve_dchg, vq_mask_dchg, step_info_dchg = machine_learning_post_process_cycle(cyc, voltage_grid,
+                                                                                                      'dchg')
+                    if vq_curve_dchg is None:
+                        print('discharge:', vq_curve_dchg)
+
                         continue
-                    #print(step_info)
-                    result.append((
+
+                    vq_curve_chg, vq_mask_chg, step_info_chg = machine_learning_post_process_cycle(cyc, voltage_grid,
+                                                                                                      'chg')
+                    if vq_curve_chg is None:
+                        print('charge:', step_info_chg)
+                        print(cyc.step_set)
+
+                        continue
+
+                    result_dchg.append((
                         cyc.get_offset_cycle(),
-                        -1.* vq_curve,
-                        vq_mask,
-                        -1.* step_info['constant_current'],
-                        step_info['end_current_prev'],
-                        step_info['end_voltage_prev'],
+                        -1.* vq_curve_dchg,
+                        vq_mask_dchg,
+                        -1.* step_info_dchg['constant_current'],
+                        step_info_dchg['end_current_prev'],
+                        step_info_dchg['end_voltage_prev'],
                         cyc.get_temperature()
                     ))
 
-            res = numpy.array(
-                result,
+                    result_chg.append((
+                        cyc.get_offset_cycle(),
+                        vq_curve_chg,
+                        vq_mask_chg,
+                        step_info_chg['constant_current'],
+                        step_info_chg['end_current_prev'],
+                        step_info_chg['end_voltage_prev'],
+                        cyc.get_temperature()
+                    ))
+
+            res_dchg = numpy.array(
+                result_dchg,
                 dtype=[
                     ('cycle_number', 'f4'),
                     ('capacity_vector', 'f4', len(voltage_grid)),
@@ -232,21 +255,36 @@ def initial_processing(my_barcodes, fit_args):
                     ('constant_current', 'f4'),
                     ('end_current_prev', 'f4'),
                     ('end_voltage_prev', 'f4'),
-
+                    ('temperature', 'f4'),
+                ])
+            res_chg = numpy.array(
+                result_chg,
+                dtype=[
+                    ('cycle_number', 'f4'),
+                    ('capacity_vector', 'f4', len(voltage_grid)),
+                    ('vq_curve_mask', 'f4', len(voltage_grid)),
+                    ('constant_current', 'f4'),
+                    ('end_current_prev', 'f4'),
+                    ('end_voltage_prev', 'f4'),
                     ('temperature', 'f4'),
                 ])
 
             cyc_grp_dict[
                 (cyc_group.charging_rate, cyc_group.discharging_rate, 'dchg')
-            ] = (res, {'avg_constant_current': numpy.average(res['constant_current']),
-                       'avg_end_current_prev': numpy.average(res['end_current_prev']),
-                       'avg_end_voltage_prev': numpy.average(res['end_voltage_prev']),}
+            ] = (res_dchg, {'avg_constant_current': numpy.average(res_dchg['constant_current']),
+                       'avg_end_current_prev': numpy.average(res_dchg['end_current_prev']),
+                       'avg_end_voltage_prev': numpy.average(res_dchg['end_voltage_prev']),}
+                 )
+            cyc_grp_dict[
+                (cyc_group.charging_rate, cyc_group.discharging_rate, 'chg')
+            ] = (res_chg, {'avg_constant_current': numpy.average(res_chg['constant_current']),
+                            'avg_end_current_prev': numpy.average(res_chg['end_current_prev']),
+                            'avg_end_voltage_prev': numpy.average(res_chg['end_voltage_prev']), }
                  )
 
         fig = plt.figure()
         ax = fig.add_subplot(111)
         for k in cyc_grp_dict.keys():
-            print(voltage_grid, numpy.average(cyc_grp_dict[k][0]['vq_curve_mask'], axis=0))
             ax.plot(voltage_grid, numpy.average(cyc_grp_dict[k][0]['vq_curve_mask'], axis=0))
         plt.savefig(os.path.join(fit_args['path_to_dataset'], 'masks_version_{}_barcode_{}.png'.format(fit_args['dataset_version'], barcode)))
         plt.close(fig)

d0af6a2351021c08ea34945cbdb646727e0c473b
Samuel Buteau <Samuel.Buteau@dal.ca>
2020-03-11 14:30:59 -0300

    documentation of the format of the ml-dataset added

--- neware_parser/management/commands/ml_smoothing.py
+++ neware_parser/management/commands/ml_smoothing.py
@@ -20,7 +20,9 @@ Shortened Variable Names:
     res -   result
 '''
 
-# TODO(sam): add end current/voltage throughout
+#TODO(sam): For each barcode, needs a multigrid of (S, V, I, T) (current needs to be adjusted)
+#TODO(sam): Each cycle must have an index mapping to the nearest reference cycle.
+#TODO(sam): to evaluate a cycle, there must be the multigrid, the reference cycle scaled by cycle number, the cell features, pasted together and ran through a neural net.
 
 NEIGH_INT_MIN_CYC_INDEX = 0
 NEIGH_INT_MAX_CYC_INDEX = 1
@@ -33,9 +35,82 @@ NEIGH_FLOAT_CHG_RATE = 1
 NEIGH_FLOAT_DCHG_RATE = 2
 
 
+#TODO(sam): these huge tensors would be much easier to understand with ragged tensors.
+# right now, I am just flattening everything.
+
 # ==== Begin: initial processing ===============================================
 
 def initial_processing(my_data, barcodes, fit_args):
+    """
+    my_data has the following structure:
+        my_data: a dictionary indexed by various data:
+            - 'voltage_grid': 1D array of voltages
+            - 'current_grid': 1D array of log currents
+            - 'temperature_grid': 1D array of temperatures
+            - 'sign_grid': 1D array of signs
+            - 'all_data': a dictionary indexed by barcode. Each barcode yields:
+                - 'all_reference_mats': structured array with dtype =
+                    [
+                        (
+                            'cycle_number',
+                            'f4'
+                        ),
+                        (
+                            'count_matrix',
+                            'f4',
+                            (
+                                len(sign_grid),
+                                len(voltage_grid_degradation),
+                                len(current_grid),
+                                len(temperature_grid)
+                            )
+                        ),
+                    ]
+
+                - 'cyc_grp_dict': we know how this works.
+                    basically groups of steps indexed by group averages of
+                    (
+                        end_current_prev,
+                        constant_current,
+                        end_current,
+                        end_voltage_prev,
+                        end_voltage,
+                        sign
+                    )
+
+                    each group is a tuple:
+                    - res, a numpy structured array with dtype:
+                        [
+                            ('cycle_number', 'f4'),
+                            ('cc_voltage_vector', 'f4', len(voltage_grid)),
+                            ('cc_capacity_vector', 'f4', len(voltage_grid)),
+                            ('cc_mask_vector', 'f4', len(voltage_grid)),
+                            ('cv_current_vector', 'f4', fit_args['current_max_n']),
+                            ('cv_capacity_vector', 'f4', fit_args['current_max_n']),
+                            ('cv_mask_vector', 'f4', fit_args['current_max_n']),
+                            ('constant_current', 'f4'),
+                            ('end_current_prev', 'f4'),
+                            ('end_current', 'f4'),
+                            ('end_voltage_prev', 'f4'),
+                            ('end_voltage', 'f4'),
+                            ('last_cc_voltage', 'f4'),
+                            ('last_cc_capacity', 'f4'),
+                            ('last_cv_capacity', 'f4'),
+                            ('temperature', 'f4'),
+                        ]
+                    - dictionary of averages:
+                        -     'avg_constant_current'
+                        -     'avg_end_current_prev'
+                        -     'avg_end_current'
+                        -     'avg_end_voltage_prev'
+                        -     'avg_end_voltage'
+                        -     'avg_last_cc_voltage'
+
+
+
+
+    """
+
     all_cells_neigh_data_int, all_cycle_nums, all_constant_current, \
     all_end_current_prev, all_end_voltage_prev, all_end_voltage = [], [], [], \
                                                                   [], [], []

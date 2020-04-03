ad8b7f8e3c8cb8a88b5de15ab5b0e8e5566f01ae
Harvey Wang <harvey@dal.ca>
2020-04-03 15:35:28 -0300

    [refact] created new PrimitiveDictionaryLayer file

--- neware_parser/DegradationModel.py
+++ neware_parser/DegradationModel.py
@@ -1,8 +1,9 @@
 import numpy
 import tensorflow as tf
 from tensorflow.keras import Model
-from tensorflow.keras.layers import Dense, Layer
+from tensorflow.keras.layers import Dense
 
+from neware_parser.PrimitiveDictionaryLayer import PrimitiveDictionaryLayer
 from neware_parser.StressToEncodedLayer import StressToEncodedLayer
 from neware_parser.loss_calculator import *
 
@@ -1866,47 +1867,3 @@ class DegradationModel(Model):
                 "pred_q_scale": q_scale,
                 "pred_shift": shift,
             }
-
-
-# stores cell features
-# key: index
-# value: feature (matrix)
-class PrimitiveDictionaryLayer(Layer):
-
-    def __init__(self, num_features, id_dict):
-        super(PrimitiveDictionaryLayer, self).__init__()
-        self.num_features = num_features
-        self.num_keys = 1 + max(id_dict.values())
-        self.id_dict = id_dict
-        self.kernel = self.add_weight(
-            "kernel", shape = [self.num_keys, self.num_features]
-        )
-        self.sample_epsilon = 0.05
-
-    def get_main_ker(self):
-        return self.kernel.numpy()
-
-    def __call__(self, input, training = True, sample = False):
-        fetched_features = tf.gather(self.kernel, input, axis = 0)
-        if training:
-            features_loss = .1 * incentive_magnitude(
-                fetched_features,
-                Target.Small,
-                Level.Proportional
-            )
-            features_loss = tf.reduce_mean(
-                features_loss,
-                axis = 1,
-                keepdims = True
-            )
-
-        else:
-            features_loss = None
-
-        if sample:
-            eps = tf.random.normal(
-                shape = [input.shape[0], self.num_features]
-            )
-            fetched_features = fetched_features + self.sample_epsilon * eps
-
-        return fetched_features, features_loss

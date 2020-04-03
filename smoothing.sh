da6bc5a123caf306665ead06751edc3a5cc84d27
Harvey Wang <harvey@dal.ca>
2020-04-03 11:52:34 -0300

    [fix] notes bug

--- smoothing.sh
+++ smoothing.sh
@@ -1,5 +1,5 @@
 [ -d $1 ] && rm -r $1
 
 mkdir $1
-echo $3 > $1/notes.txt
+echo "$3" > $1/notes.txt
 python3 manage.py ml_smoothing --path_to_dataset compiled_datasets1 --dataset_version $2 --path_to_plots=$1

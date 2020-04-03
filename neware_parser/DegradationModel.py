182dbd6b1f1cdcc1b87e202127819b40fc66a55c
Harvey Wang <harvey@dal.ca>
2020-04-03 15:07:13 -0300

    [refact] separated out some loss

--- neware_parser/DegradationModel.py
+++ neware_parser/DegradationModel.py
@@ -300,8 +300,278 @@ def get_norm_cycle(params):
     )
 
 
+def calculate_projection_loss(
+    sampled_latent, sampled_pos, sampled_neg,
+    predicted_pos, predicted_neg
+):
+    return incentive_combine([
+        (
+            10.,
+            (1. - sampled_latent) * incentive_inequality(
+                sampled_pos, Inequality.Equals, predicted_pos,
+                Level.Proportional
+            )
+        ),
+        (
+            10.,
+            (1. - sampled_latent) * incentive_inequality(
+                sampled_neg, Inequality.Equals, predicted_neg,
+                Level.Proportional
+            )
+        ),
+    ])
+
+
+def calculate_oob_loss(reciprocal_q):
+    return incentive_combine([
+        (
+            1.,
+            incentive_inequality(
+                reciprocal_q, Inequality.GreaterThan, 0., Level.Strong
+            )
+        ),
+        (
+            1.,
+            incentive_inequality(
+                reciprocal_q, Inequality.LessThan, 1., Level.Strong
+            )
+        ),
+    ])
+
+
+def calculate_reciprocal_loss(
+    sampled_voltages, sampled_qs,
+    v_plus, v_minus, v_plus_der, v_minus_der,
+    reciprocal_v, reciprocal_q,
+):
+    return incentive_combine([
+        (
+            2.,
+            incentive_inequality(
+                sampled_voltages, Inequality.Equals, reciprocal_v,
+                Level.Proportional
+            )
+        ),
+        (
+            2.,
+            incentive_inequality(
+                sampled_qs, Inequality.Equals, reciprocal_q, Level.Proportional
+            )
+        ),
+        (
+            .01,
+            incentive_magnitude(
+                v_minus, Target.Small, Level.Proportional
+            )
+        ),
+        (
+            .01,
+            incentive_magnitude(
+                v_plus, Target.Small, Level.Proportional
+            )
+        ),
+        (
+            10.,
+            incentive_inequality(
+                v_minus, Inequality.GreaterThan, 0., Level.Strong
+            )
+        ),
+        (
+            10.,
+            incentive_inequality(
+                v_minus, Inequality.LessThan, 5., Level.Strong
+            )
+        ),
+        (
+            1.,
+            incentive_inequality(
+                v_minus_der['d_q'], Inequality.LessThan, 0., Level.Strong
+            )
+        ),
+
+        (
+            10.,
+            incentive_inequality(
+                v_plus, Inequality.GreaterThan, 0., Level.Strong
+            )
+        ),
+        (
+            10.,
+            incentive_inequality(
+                v_plus, Inequality.LessThan, 5., Level.Strong
+            )
+        ),
+        (
+            1.,
+            incentive_inequality(
+                v_plus_der['d_q'], Inequality.GreaterThan, 0., Level.Strong
+            )
+        ),
+
+    ])
+
+
+def calculate_q_loss(q, q_der):
+    return incentive_combine([
+        (
+            1.,
+            incentive_magnitude(
+                q,
+                Target.Small,
+                Level.Proportional
+            )
+        ),
+        (
+            100.,
+            incentive_inequality(
+                q, Inequality.GreaterThan, 0,
+                Level.Strong
+            )
+        ),
+        (
+            10000.,
+            incentive_inequality(
+                q_der['d_voltage'], Inequality.GreaterThan, 0.01,
+                Level.Strong
+            )
+        ),
+        (
+            100.,
+            incentive_magnitude(
+                q_der['d3_voltage'],
+                Target.Small,
+                Level.Proportional
+            )
+        ),
+        (
+            .01,
+            incentive_magnitude(
+                q_der['d_features'],
+                Target.Small,
+                Level.Proportional
+            )
+        ),
+        (
+            .01,
+            incentive_magnitude(
+                q_der['d2_features'],
+                Target.Small,
+                Level.Strong
+            )
+        ),
+        (
+            100.,
+            incentive_magnitude(
+                q_der['d_shift'],
+                Target.Small,
+                Level.Proportional
+            )
+        ),
+
+        (
+            100.,
+            incentive_magnitude(
+                q_der['d2_shift'],
+                Target.Small,
+                Level.Proportional
+            )
+        ),
+        (
+            100.,
+            incentive_magnitude(
+                q_der['d3_shift'],
+                Target.Small,
+                Level.Proportional
+            )
+        ),
+    ])
+
+
+def calculate_q_scale_loss(q_scale, q_scale_der):
+    return incentive_combine([
+        (
+            10000.,
+            incentive_inequality(
+                q_scale, Inequality.GreaterThan, 0.1,
+                Level.Strong
+            )
+        ),
+        (
+            20000.,
+            incentive_inequality(
+                q_scale, Inequality.LessThan, 1.2,
+                Level.Strong
+            )
+        ),
+
+        (
+            10.,
+            incentive_inequality(
+                q_scale, Inequality.Equals, 1,
+                Level.Proportional
+            )
+        ),
+        (
+            1.,
+            incentive_inequality(
+                q_scale_der['d_cycle'], Inequality.LessThan, 0,
+                Level.Proportional
+            )
+        ),
+        (
+            .1,
+            incentive_inequality(
+                q_scale_der['d2_cycle'], Inequality.LessThan, 0,
+                Level.Proportional
+            )
+        ),
+        (
+            100.,
+            incentive_magnitude(
+                q_scale_der['d_cycle'],
+                Target.Small,
+                Level.Proportional
+            )
+        ),
+
+        (
+            100.,
+            incentive_magnitude(
+                q_scale_der['d2_cycle'],
+                Target.Small,
+                Level.Proportional
+            )
+        ),
+        (
+            100.,
+            incentive_magnitude(
+                q_scale_der['d3_cycle'],
+                Target.Small,
+                Level.Proportional
+            )
+        ),
+
+        (
+            1.,
+            incentive_magnitude(
+                q_scale_der['d_features'],
+                Target.Small,
+                Level.Proportional
+            )
+        ),
+        (
+            1.,
+            incentive_magnitude(
+                q_scale_der['d2_features'],
+                Target.Small,
+                Level.Strong
+            )
+        ),
+    ])
+
+
 def calculate_shift_loss(shift, shift_der):
-    incentive_combine([
+    return incentive_combine([
         (
             10000.,
             incentive_inequality(
@@ -1924,31 +2194,17 @@ class DegradationModel(Model):
             ) = self.sample(svit_grid, batch_count, count_matrix)
 
             predicted_pos = self.pos_projection_direct(
-                cell_features = sampled_cell_features,
-                training = training
+                cell_features = sampled_cell_features, training = training
             )
 
             predicted_neg = self.neg_projection_direct(
-                cell_features = sampled_cell_features,
-                training = training
+                cell_features = sampled_cell_features, training = training
             )
 
-            projection_loss = .1 * incentive_combine([
-                (
-                    10.,
-                    (1. - sampled_latent) * incentive_inequality(
-                        sampled_pos, Inequality.Equals, predicted_pos,
-                        Level.Proportional
-                    )
-                ),
-                (
-                    10.,
-                    (1. - sampled_latent) * incentive_inequality(
-                        sampled_neg, Inequality.Equals, predicted_neg,
-                        Level.Proportional
-                    )
-                ),
-            ])
+            projection_loss = .1 * calculate_projection_loss(
+                sampled_latent, sampled_pos, sampled_neg,
+                predicted_pos, predicted_neg
+            )
 
             reciprocal_q, oob_loss_1 = self.reciprocal_q(
                 params = {
@@ -1981,102 +2237,16 @@ class DegradationModel(Model):
                     'q': sampled_qs,
                     'features': sampled_features,
                 },
-                der_params = {
-                    'q': 1,
-                }
+                der_params = {'q': 1}
             )
 
-            oob_loss_3 = incentive_combine([
-                (
-                    1.,
-                    incentive_inequality(
-                        reciprocal_q, Inequality.GreaterThan, 0., Level.Strong
-                    )
-                ),
-                (
-                    1.,
-                    incentive_inequality(
-                        reciprocal_q, Inequality.LessThan, 1., Level.Strong
-                    )
-                ),
-            ])
+            oob_loss_3 = calculate_oob_loss(reciprocal_q)
 
-            reciprocal_loss = .5 * incentive_combine([
-                (
-                    2.,
-                    incentive_inequality(
-                        sampled_voltages, Inequality.Equals, reciprocal_v,
-                        Level.Proportional
-                    )
-                ),
-                (
-                    2.,
-                    incentive_inequality(
-                        sampled_qs, Inequality.Equals, reciprocal_q,
-                        Level.Proportional
-                    )
-                ),
-                (
-                    .01,
-                    incentive_magnitude(
-                        v_minus, Target.Small, Level.Proportional
-                    )
-                ),
-                (
-                    .01,
-                    incentive_magnitude(
-                        v_plus, Target.Small, Level.Proportional
-                    )
-                ),
-
-                (
-                    10.,
-                    incentive_inequality(
-                        v_minus, Inequality.GreaterThan, 0.,
-                        Level.Strong
-                    )
-                ),
-
-                (
-                    10.,
-                    incentive_inequality(
-                        v_minus, Inequality.LessThan, 5.,
-                        Level.Strong
-                    )
-                ),
-
-                (
-                    1.,
-                    incentive_inequality(
-                        v_minus_der['d_q'], Inequality.LessThan, 0.,
-                        Level.Strong
-                    )
-                ),
-
-                (
-                    10.,
-                    incentive_inequality(
-                        v_plus, Inequality.GreaterThan, 0.,
-                        Level.Strong
-                    )
-                ),
-                (
-                    10.,
-                    incentive_inequality(
-                        v_plus, Inequality.LessThan, 5.,
-                        Level.Strong
-                    )
-                ),
-
-                (
-                    1.,
-                    incentive_inequality(
-                        v_plus_der['d_q'], Inequality.GreaterThan, 0.,
-                        Level.Strong
-                    )
-                ),
-
-            ])
+            reciprocal_loss = .5 * calculate_reciprocal_loss(
+                sampled_voltages, sampled_qs,
+                v_plus, v_minus, v_plus_der, v_minus_der,
+                reciprocal_v, reciprocal_q,
+            )
 
             q, q_der = self.create_derivatives(
                 self.q_for_derivative,
@@ -2088,79 +2258,7 @@ class DegradationModel(Model):
                 der_params = {'voltage': 3, 'features': 2, 'shift': 3}
             )
 
-            q_loss = .0001 * incentive_combine([
-                (
-                    1.,
-                    incentive_magnitude(
-                        q,
-                        Target.Small,
-                        Level.Proportional
-                    )
-                ),
-                (
-                    100.,
-                    incentive_inequality(
-                        q, Inequality.GreaterThan, 0,
-                        Level.Strong
-                    )
-                ),
-                (
-                    10000.,
-                    incentive_inequality(
-                        q_der['d_voltage'], Inequality.GreaterThan, 0.01,
-                        Level.Strong
-                    )
-                ),
-                (
-                    100.,
-                    incentive_magnitude(
-                        q_der['d3_voltage'],
-                        Target.Small,
-                        Level.Proportional
-                    )
-                ),
-                (
-                    .01,
-                    incentive_magnitude(
-                        q_der['d_features'],
-                        Target.Small,
-                        Level.Proportional
-                    )
-                ),
-                (
-                    .01,
-                    incentive_magnitude(
-                        q_der['d2_features'],
-                        Target.Small,
-                        Level.Strong
-                    )
-                ),
-                (
-                    100.,
-                    incentive_magnitude(
-                        q_der['d_shift'],
-                        Target.Small,
-                        Level.Proportional
-                    )
-                ),
-
-                (
-                    100.,
-                    incentive_magnitude(
-                        q_der['d2_shift'],
-                        Target.Small,
-                        Level.Proportional
-                    )
-                ),
-                (
-                    100.,
-                    incentive_magnitude(
-                        q_der['d3_shift'],
-                        Target.Small,
-                        Level.Proportional
-                    )
-                ),
-            ])
+            q_loss = .0001 * calculate_q_loss(q, q_der)
 
             q_scale, q_scale_der = self.create_derivatives(
                 self.q_scale_for_derivative,
@@ -2174,86 +2272,7 @@ class DegradationModel(Model):
                 der_params = {'cycle': 3, 'current': 0, 'features': 2}
             )
 
-            q_scale_loss = .0001 * incentive_combine([
-                (
-                    10000.,
-                    incentive_inequality(
-                        q_scale, Inequality.GreaterThan, 0.1,
-                        Level.Strong
-                    )
-                ),
-                (
-                    20000.,
-                    incentive_inequality(
-                        q_scale, Inequality.LessThan, 1.2,
-                        Level.Strong
-                    )
-                ),
-
-                (
-                    10.,
-                    incentive_inequality(
-                        q_scale, Inequality.Equals, 1,
-                        Level.Proportional
-                    )
-                ),
-                (
-                    1.,
-                    incentive_inequality(
-                        q_scale_der['d_cycle'], Inequality.LessThan, 0,
-                        Level.Proportional
-                    )
-                ),
-                (
-                    .1,
-                    incentive_inequality(
-                        q_scale_der['d2_cycle'], Inequality.LessThan, 0,
-                        Level.Proportional
-                    )
-                ),
-                (
-                    100.,
-                    incentive_magnitude(
-                        q_scale_der['d_cycle'],
-                        Target.Small,
-                        Level.Proportional
-                    )
-                ),
-
-                (
-                    100.,
-                    incentive_magnitude(
-                        q_scale_der['d2_cycle'],
-                        Target.Small,
-                        Level.Proportional
-                    )
-                ),
-                (
-                    100.,
-                    incentive_magnitude(
-                        q_scale_der['d3_cycle'],
-                        Target.Small,
-                        Level.Proportional
-                    )
-                ),
-
-                (
-                    1.,
-                    incentive_magnitude(
-                        q_scale_der['d_features'],
-                        Target.Small,
-                        Level.Proportional
-                    )
-                ),
-                (
-                    1.,
-                    incentive_magnitude(
-                        q_scale_der['d2_features'],
-                        Target.Small,
-                        Level.Strong
-                    )
-                ),
-            ])
+            q_scale_loss = .0001 * calculate_q_scale_loss(q_scale, q_scale_der)
 
             shift, shift_der = self.create_derivatives(
                 self.shift_for_derivative,

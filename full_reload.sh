ada96bb05ea84fe820f6b1472b063bd987084ae8
Harvey Wang <harvey@dal.ca>
2020-04-14 20:11:02 -0300

    deleted unused scripts

--- full_reload.sh
+++
@@ -1,7 +0,0 @@
-rm db.sqlite3
-rm -f neware_parser/migrations/*
-touch neware_parser/migrations/__init__.py
-python3 manage.py makemigrations
-python3 manage.py migrate
-python3 manage.py import_raw_data --path_to_file=NEWARE_Degradation_Analysis
-python3 manage.py process_raw_data

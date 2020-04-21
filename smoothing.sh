c121ffdd5f17750c6cffb763d9723e738e9e5863
Samuel Buteau <Samuel.Buteau@dal.ca>
2020-04-21 15:39:58 -0300

    first try, supervising just the anode

--- smoothing.sh
+++ smoothing.sh
@@ -1,1 +1,1 @@
-python3 manage.py ml_smoothing --path_to_dataset compiled_datasets1 --dataset_version $2 --path_to_plots=$1
+python3 manage.py ml_smoothing --path_to_dataset compiled_datasets1 --dataset_version $2 --path_to_plots=$1 --path_v_curves v_curves

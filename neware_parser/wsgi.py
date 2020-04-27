c32a6944951c69d8486c8769987c8fd57d55d98c
Harvey Wang <harvey@dal.ca>
2020-04-27 10:03:46 -0300

    Update wsgi.py

--- neware_parser/wsgi.py
+++ neware_parser/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'neware_parser.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "neware_parser.settings")
 
 application = get_wsgi_application()

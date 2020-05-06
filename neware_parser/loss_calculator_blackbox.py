732addb43ce7e49498ebe7bef1dbc12ce311e425
Harvey Wang <harvey@dal.ca>
2020-05-06 09:38:09 -0300

    cleanup

--- neware_parser/loss_calculator_blackbox.py
+++ neware_parser/loss_calculator_blackbox.py
@@ -2,94 +2,52 @@ from neware_parser.Key import Key
 from neware_parser.incentives import *
 
 
-
 def calculate_q_loss(q, q_der, incentive_coeffs):
     return incentive_combine([
 
         (
             incentive_coeffs[Key.COEFF_Q_GEQ],
-            incentive_inequality(
-                q, Inequality.GreaterThan, 0,
-                Level.Strong
-            )
-        ),
-        (
+            incentive_inequality(q, Inequality.GreaterThan, 0, Level.Strong)
+        ), (
             incentive_coeffs[Key.COEFF_Q_LEQ],
-            incentive_inequality(
-                q, Inequality.LessThan, 1,
-                Level.Strong
-            )
-        ),
-
-
-        (
+            incentive_inequality(q, Inequality.LessThan, 1, Level.Strong)
+        ), (
             incentive_coeffs[Key.COEFF_Q_V_MONO],
             incentive_inequality(
-                q_der["d_v"], Inequality.GreaterThan, 0,
-                Level.Strong
+                q_der["d_v"], Inequality.GreaterThan, 0, Level.Strong,
             )
-        ),
-        (
+        ), (
             incentive_coeffs[Key.COEFF_Q_DER3_V],
-            incentive_magnitude(
-                q_der["d3_v"],
-                Target.Small,
-                Level.Proportional
-            )
-        ),
-
-        (
+            incentive_magnitude(q_der["d3_v"], Target.Small, Level.Proportional)
+        ), (
             incentive_coeffs[Key.COEFF_Q_DER3_I],
             incentive_magnitude(
-                q_der["d3_current"],
-                Target.Small,
-                Level.Proportional
+                q_der["d3_current"], Target.Small, Level.Proportional,
             )
-        ),
-
-        (
+        ), (
             incentive_coeffs[Key.COEFF_Q_DER3_N],
             incentive_magnitude(
-                q_der["d3_cycle"],
-                Target.Small,
-                Level.Proportional
+                q_der["d3_cycle"], Target.Small, Level.Proportional,
             )
-        ),
-
-        (
+        ), (
             incentive_coeffs[Key.COEFF_Q_DER_I],
             incentive_magnitude(
-                q_der["d_current"],
-                Target.Small,
-                Level.Proportional
+                q_der["d_current"], Target.Small, Level.Proportional,
             )
-        ),
-
-        (
+        ), (
             incentive_coeffs[Key.COEFF_Q_DER_N],
             incentive_magnitude(
-                q_der["d_cycle"],
-                Target.Small,
-                Level.Proportional
+                q_der["d_cycle"], Target.Small, Level.Proportional,
             )
-        ),
-
-        (
+        ), (
             incentive_coeffs[Key.COEFF_FEAT_CELL_DER],
             incentive_magnitude(
-                q_der["d_features_cell"],
-                Target.Small,
-                Level.Proportional
+                q_der["d_features_cell"], Target.Small, Level.Proportional,
             )
-        ),
-        (
+        ), (
             incentive_coeffs[Key.COEFF_FEAT_CELL_DER2],
             incentive_magnitude(
-                q_der["d2_features_cell"],
-                Target.Small,
-                Level.Strong
+                q_der["d2_features_cell"], Target.Small, Level.Strong,
             )
         ),
-
     ])
-

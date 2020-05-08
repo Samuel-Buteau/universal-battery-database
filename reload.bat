19eda3fc810a709e0881514d9377cd8262551fc8
Samuel Buteau <Samuel.Buteau@dal.ca>
2020-05-08 16:36:12 -0300

    made the setup easier

--- reload.bat
+++ reload.bat
@@ -19,14 +19,14 @@ copy NUL machine_learning\migrations\__init__.py
 python manage.py makemigrations
 python manage.py migrate
 
-python manage.py edit_database_filenamehelper  --mode add_category
-python manage.py edit_database_filenamehelper  --mode add_charger_drive_profile
-python manage.py edit_database_filenamehelper  --mode add_experiment_type
-python manage.py edit_database_filenamehelper --mode display --model Category
-python manage.py edit_database_filenamehelper --mode display --model ChargerDriveProfile
-python manage.py edit_database_filenamehelper --mode display --model ExperimentType
+python manage.py edit_database_filename_database  --mode add_category
+python manage.py edit_database_filename_database  --mode add_charger_drive_profile
+python manage.py edit_database_filename_database  --mode add_experiment_type
+python manage.py edit_database_filename_database --mode display --model Category
+python manage.py edit_database_filename_database --mode display --model ChargerDriveProfile
+python manage.py edit_database_filename_database --mode display --model ExperimentType
 
 
-python manage.py edit_database_filenamehelper --mode just_add_files --data_dir=%3
-python manage.py edit_database_filenamehelper --mode just_parse_database_files
+python manage.py edit_database_filename_database --mode just_add_files --data_dir=%3
+python manage.py edit_database_filename_database --mode just_parse_database_files
 python manage.py import_and_process_raw_neware --DEBUG

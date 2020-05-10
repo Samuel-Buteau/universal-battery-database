9f84c15ec58567aa3485cb75c0097132b94b9f51
Harvey Wang <harvey@dal.ca>
2020-05-10 08:18:02 -0300

    replace Keys

--- machine_learning/loss_calculator.py
+++ machine_learning/loss_calculator.py
@@ -144,18 +144,18 @@ def calculate_q_loss(q, q_der, incentive_coeffs):
             incentive_coeffs["coeff_q_small"],
             incentive_magnitude(q, Target.Small, Level.Proportional),
         ), (
-            incentive_coeffs["coeff_q_geq"],
+            incentive_coeffs[Key.COEFF_Q_GEQ],
             incentive_inequality(q, Inequality.GreaterThan, 0, Level.Strong),
         ), (
-            incentive_coeffs["coeff_q_leq"],
+            incentive_coeffs[Key.COEFF_Q_LEQ],
             incentive_inequality(q, Inequality.LessThan, 1, Level.Strong),
         ), (
-            incentive_coeffs["coeff_q_v_mono"],
+            incentive_coeffs[Key.COEFF_Q_V_MONO],
             incentive_inequality(
                 q_der["d_v"], Inequality.GreaterThan, 0, Level.Strong,
             ),
         ), (
-            incentive_coeffs["coeff_q_d3_v"],
+            incentive_coeffs[Key.COEFF_Q_DER3_V],
             incentive_magnitude(
                 q_der["d3_v"], Target.Small, Level.Proportional,
             ),
@@ -165,22 +165,22 @@ def calculate_q_loss(q, q_der, incentive_coeffs):
                 q_der["d3_shift"], Target.Small, Level.Proportional,
             ),
         ), (
-            incentive_coeffs["coeff_q_d3_current"],
+            incentive_coeffs[Key.COEFF_Q_DER3_I],
             incentive_magnitude(
                 q_der["d3_current"], Target.Small, Level.Proportional,
             ),
         ), (
-            incentive_coeffs["coeff_q_d3_cycle"],
+            incentive_coeffs[Key.COEFF_Q_DER3_N],
             incentive_magnitude(
                 q_der["d3_cycle"], Target.Small, Level.Proportional,
             ),
         ), (
-            incentive_coeffs["coeff_q_d_current"],
+            incentive_coeffs[Key.COEFF_Q_DER_I],
             incentive_magnitude(
                 q_der["d_current"], Target.Small, Level.Proportional,
             ),
         ), (
-            incentive_coeffs["coeff_q_d_cycle"],
+            incentive_coeffs[Key.COEFF_Q_DER_N],
             incentive_magnitude(
                 q_der["d_cycle"], Target.Small, Level.Proportional,
             ),
--- machine_learning/loss_calculator_blackbox.py
+++ machine_learning/loss_calculator_blackbox.py
@@ -1,42 +1,43 @@
 from machine_learning.incentives import *
+from Key import Key
 
 
 def calculate_q_loss(q, q_der, incentive_coeffs):
     return incentive_combine([
 
         (
-            incentive_coeffs["coeff_q_geq"],
+            incentive_coeffs[Key.COEFF_Q_GEQ],
             incentive_inequality(q, Inequality.GreaterThan, 0, Level.Strong),
         ), (
-            incentive_coeffs["coeff_q_leq"],
+            incentive_coeffs[Key.COEFF_Q_LEQ],
             incentive_inequality(q, Inequality.LessThan, 1, Level.Strong),
         ), (
-            incentive_coeffs["coeff_q_v_mono"],
+            incentive_coeffs[Key.COEFF_Q_V_MONO],
             incentive_inequality(
                 q_der["d_v"], Inequality.GreaterThan, 0, Level.Strong,
             ),
         ), (
-            incentive_coeffs["coeff_q_d3_v"],
+            incentive_coeffs[Key.COEFF_Q_DER3_V],
             incentive_magnitude(
                 q_der["d3_v"], Target.Small, Level.Proportional,
             ),
         ), (
-            incentive_coeffs["coeff_q_d3_current"],
+            incentive_coeffs[Key.COEFF_Q_DER3_I],
             incentive_magnitude(
                 q_der["d3_current"], Target.Small, Level.Proportional,
             ),
         ), (
-            incentive_coeffs["coeff_q_d3_cycle"],
+            incentive_coeffs[Key.COEFF_Q_DER3_N],
             incentive_magnitude(
                 q_der["d3_cycle"], Target.Small, Level.Proportional,
             ),
         ), (
-            incentive_coeffs["coeff_q_d_current"],
+            incentive_coeffs[Key.COEFF_Q_DER_I],
             incentive_magnitude(
                 q_der["d_current"], Target.Small, Level.Proportional,
             ),
         ), (
-            incentive_coeffs["coeff_q_d_cycle"],
+            incentive_coeffs[Key.COEFF_Q_DER_N],
             incentive_magnitude(
                 q_der["d_cycle"], Target.Small, Level.Proportional,
             ),

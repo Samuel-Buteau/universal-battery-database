97014351baa1f4715cbf6abfa1492bc0f677d294
Harvey Wang <harvey@dal.ca>
2020-05-16 11:54:27 -0300

    settings.py and urls.py

--- cycling/settings.py
+++ cycling/settings.py
@@ -12,12 +12,12 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import configparser
+
 config = configparser.ConfigParser()
 config.read("cycling/config.ini")
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -29,7 +29,6 @@ SECRET_KEY = config["DEFAULT"]["SecretKey"]
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -77,23 +76,20 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "cycling.wsgi.application"
 
-
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 
-
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends."+config["DEFAULT"]["Backend"],
+        "ENGINE": "django.db.backends." + config["DEFAULT"]["Backend"],
         "NAME": config["DEFAULT"]["Database"],
-        "USER":config["DEFAULT"]["User"],
-        "PASSWORD":config["DEFAULT"]["Password"],
-        "HOST":config["DEFAULT"]["Host"],
-        "PORT":config["DEFAULT"]["Port"],
+        "USER": config["DEFAULT"]["User"],
+        "PASSWORD": config["DEFAULT"]["Password"],
+        "HOST": config["DEFAULT"]["Host"],
+        "PORT": config["DEFAULT"]["Port"],
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -101,18 +97,14 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
-    },
-    {
+    }, {
         "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
-    },
-    {
+    }, {
         "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
-    },
-    {
+    }, {
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
@@ -126,7 +118,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/

214e2b80f7688b89448c1eb31e233838e08cf784
Samuel Buteau <Samuel.Buteau@dal.ca>
2020-05-26 21:11:08 -0300

    bug fix

--- cycling/plot.py
+++ cycling/plot.py
@@ -109,17 +109,85 @@ def make_file_legends_and_vertical(
 
 
 
+
 def compute_from_database(
     cell_id, lower_cycle = None, upper_cycle = None, valid = True,
 ):
+    rules = {}
+    for cycle_group in get_discharge_groups_from_cell_id(cell_id):
+        rules[(
+            cycle_group.constant_rate, cycle_group.end_rate_prev,
+            cycle_group.end_rate, cycle_group.end_voltage,
+            cycle_group.end_voltage_prev, cycle_group.polarity,
+        )]={
+            DISCHARGE:{'direct_id':cycle_group.id},
+        }
+
+    field_request = [
+        (Key.N, 'f4', "CYCLE_NUMBER", None),
+        ("last_cc_capacity", 'f4', "CUSTOM", lambda cyc: -cyc.dchg_total_capacity),
+    ]
+    return compute_from_database2(cell_id, rules, field_request, lower_cycle, upper_cycle, valid)
+
+
+def generic_field(tp, f, cyc, offset_cycle=None, cell_first_time=None):
+    if tp == "CUSTOM":
+        return f(cyc)
+    elif tp == "CYCLE_NUMBER":
+        return float(cyc.cycle_number + offset_cycle)
+    elif tp == "CUMULATIVE_TIME":
+        return (cyc.get_start_time() - cell_first_time).total_seconds() / (60. * 60.)
+
+
+
+
+def compute_from_database2(
+    cell_id, rules, field_request, lower_cycle = None, upper_cycle = None, valid = True,
+):
+    """
+
+
+    Args:
+        cell_id:
+        rules: a dictionary where the key can be anything. the value is a dictionary which can have
+           DISCHARGE: a dictionary which contains
+                 'no_group':True/False,
+                 'direct_id': the id of a perfect match
+                 'constant_rate': (None/val,None/val)
+                 'end_rate': ..
+                 'end_rate_prev': ..
+                 'end_voltage': ..
+                 'end_voltage_prev': ..
+           CHARGE: a dictionary which contains
+                 'no_group':True/False,
+                 'constant_rate': (None/val,None/val)
+                 'end_rate': ..
+                 'end_rate_prev': ..
+                 'end_voltage': ..
+                 'end_voltage_prev': ..
+        field_request: a list of tuples containing:
+             (
+                key (the key to use in dtype),
+                return_type (the type used in dtype)
+                function_type (either "CUSTOM" or a known type)
+                a function in case type was custom (the function takes cyc as input)
+             )
+        lower_cycle:
+        upper_cycle:
+        valid:
+
+    Returns:
+
+    """
     files_cell_id = CyclingFile.objects.filter(
         database_file__deprecated = False,
         database_file__valid_metadata__cell_id = cell_id,
     ).order_by("database_file__last_modified")
 
-    polarity = DISCHARGE
+    cell_first_time = get_cell_id_first_time(cell_id)
+
     groups = {}
-    for cycle_group in get_discharge_groups_from_cell_id(cell_id):
+    for rule_id in rules.keys():
         q_curves = []
         for f in files_cell_id:
             offset_cycle = f.database_file.valid_metadata.start_cycle
@@ -131,34 +199,106 @@ def compute_from_database(
                     ),
                 )
 
-            if polarity == DISCHARGE:
-                filters = Q(discharge_group = cycle_group) & filters
-            elif polarity == CHARGE:
-                filters = Q(charge_group = cycle_group) & filters
+            rule = rules[rule_id]
+
+            for polarity, group_is_none, group_id_f, group_in_f in [
+                (
+                    DISCHARGE,
+                    Q(discharge_group=None),
+                    lambda x: Q(discharge_group__id=x),
+                    lambda x: Q(discharge_group__in=x),
+                ),
+                (
+                    CHARGE,
+                    Q(charge_group=None),
+                    lambda x: Q(charge_group__id=x),
+                    lambda x: Q(charge_group__in=x),
+                ),
+            ]:
+                if polarity in rule.keys():
+                    if 'no_group' in rule[polarity].keys() and rule[polarity]['no_group']:
+                        filters = group_is_none & filters
+                    elif 'direct_id' in rule[polarity].keys():
+                        filters = group_id_f(rule[polarity]['direct_id']) & filters
+                    else:
+                        group_filter = Q(cell_id=cell_id, polarity=polarity)
+                        total_capacity = 1.
+                        if CellGlobals.objects.filter(cell_id=cell_id).exists():
+                            total_capacity = CellGlobals.objects.get(cell_id=cell_id).theoretical_capacity
+                        for lab, leq_f, geq_f, range_f in [
+                            (
+                                'constant_rate',
+                                lambda x: Q(constant_current__leq=x*total_capacity),
+                                lambda x: Q(constant_current__geq=x*total_capacity),
+                                lambda x: Q(constant_current__range=(x[0]*total_capacity, x[1]*total_capacity)),
+                            ),
+                            (
+                                'end_rate',
+                                lambda x: Q(end_current__leq=x*total_capacity),
+                                lambda x: Q(end_current__geq=x*total_capacity),
+                                lambda x: Q(end_current__range=(x[0]*total_capacity, x[1]*total_capacity)),
+                            ),
+                            (
+                                'end_rate_prev',
+                                lambda x: Q(end_current_prev__leq=x*total_capacity),
+                                lambda x: Q(end_current_prev__geq=x*total_capacity),
+                                lambda x: Q(end_current_prev__range=(x[0]*total_capacity, x[1]*total_capacity)),
+                            ),
+                            (
+                                'end_voltage',
+                                lambda x: Q(end_voltage__leq=x),
+                                lambda x: Q(end_voltage__geq=x),
+                                lambda x: Q(end_voltage__range=x),
+                            ),
+                            (
+                                'end_voltage_prev',
+                                lambda x: Q(end_voltage_prev__leq=x),
+                                lambda x: Q(end_voltage_prev__geq=x),
+                                lambda x: Q(end_voltage_prev__range=x),
+                            ),
+
+                        ]:
+                            if lab in rule[polarity].keys():
+                                if rule[polarity][lab][0] is None:
+                                    group_filter = group_filter & leq_f(rule[polarity][lab][1])
+                                elif rule[polarity][lab][1] is None:
+                                    group_filter = group_filter & geq_f(rule[polarity][lab][0])
+                                else:
+                                    group_filter = group_filter & range_f(rule[polarity][lab])
+
+                        good_groups = CycleGroup.objects.filter(group_filter)
+                        if len(good_groups) == 0:
+                            filters = group_is_none & filters
+                        else:
+                            filters = group_in_f(good_groups) & filters
+
             cycles = Cycle.objects.filter(filters)
             if cycles.exists():
+
                 q_curves += list([
-                    (
-                        float(cyc.cycle_number + offset_cycle),
-                        -cyc.dchg_total_capacity,
+                    tuple(
+                        [
+                            generic_field(tp, f, cyc, offset_cycle, cell_first_time)
+                            for _, _, tp, f in field_request
+                        ]
                     )
                     for cyc in cycles.order_by("cycle_number")
-                ])
+                    ]
+                )
 
         if len(q_curves) > 0:
-            groups[(
-                cycle_group.constant_rate, cycle_group.end_rate_prev,
-                cycle_group.end_rate, cycle_group.end_voltage,
-                cycle_group.end_voltage_prev, cycle_group.polarity,
-            )] = np.array(
+            groups[rule_id] = np.array(
                 q_curves,
                 dtype = [
-                    (Key.N, 'f4'),
-                    ("last_cc_capacity", 'f4'),
+                    (key, return_type)
+                    for key, return_type, _, _ in field_request
                 ],
             )
 
     return groups
+
+
+
 
 
 def data_engine_database(

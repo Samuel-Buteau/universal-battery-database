3f02b2f65a5d039c4210d2fd7426f37810bc7e47
Samuel Buteau <Samuel.Buteau@dal.ca>
2020-06-07 21:37:09 -0300

    csv error logs

--- cycling/management/commands/import_and_process_raw_neware.py
+++ cycling/management/commands/import_and_process_raw_neware.py
@@ -1,7 +1,7 @@
 from django.core.management.base import BaseCommand
 from cycling.neware_processing_functions import *
 from datetime import date
-
+import csv
 
 def import_and_process(args):
     path_to_log = None
@@ -11,11 +11,9 @@ def import_and_process(args):
         )
         if not os.path.exists(path_to_log):
             os.makedirs(path_to_log)
-
         today = date.today()
         today_string = today.strftime("%y_%m_%d")
-
-
+        
     cell_ids = None
     for _ in range(5):
         print()
@@ -33,9 +31,17 @@ def import_and_process(args):
         print("ERRORS IN BULK IMPORT: {}".format(errors))
 
         if path_to_log is not None:
-            with open(os.path.join(path_to_log, "bulk_import_errors_{}.txt".format(today_string)),"w") as file:
+            with open(os.path.join(path_to_log, "bulk_import_errors_{}.csv".format(today_string)),"w") as file:
+                all_keys = []
                 for error in errors:
-                    file.write(str(error))
+                    all_keys.append(error.keys())
+                all_keys = list(set(all_keys))
+                writer = csv.DictWriter(file, fieldnames=all_keys)
+                writer.writeheader()
+                for error in errors:
+                    writer.writerow(
+                        {k: str(v) for k, v in error.items()}
+                    )
 
 
     for _ in range(5):
@@ -48,9 +54,18 @@ def import_and_process(args):
     if len(errors) > 0:
         print("ERRORS IN BULK PROCESS: {}".format(errors))
         if path_to_log is not None:
-            with open(os.path.join(path_to_log, "bulk_process_errors_{}.txt".format(today_string)),"w") as file:
+            with open(os.path.join(path_to_log, "bulk_process_errors_{}.csv".format(today_string)),"w") as file:
+                all_keys = []
                 for error in errors:
-                    file.write(str(error))
+                    all_keys.append(error.keys())
+                all_keys = list(set(all_keys))
+                writer = csv.DictWriter(file, fieldnames=all_keys)
+                writer.writeheader()
+                for error in errors:
+                    writer.writerow(
+                        {k: str(v) for k, v in error.items()}
+                    )
+
 
 class Command(BaseCommand):
     def add_arguments(self, parser):

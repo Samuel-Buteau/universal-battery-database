9ed4a0a5457d4ed411731d788672582032437f68
Samuel <Samuel.Buteau@dal.ca>
2020-06-12 15:27:52 -0300

    updated load_parse_files

--- load_parse_files.sh
+++ load_parse_files.sh
@@ -1,3 +1,3 @@
 python3 manage.py edit_database_filename_database --mode just_add_files --data_dir=$1
 python3 manage.py edit_database_filename_database --mode just_parse_database_files
-python3 manage.py import_and_process_raw_neware ----NO_DEBUG
+python3 manage.py import_and_process_raw_neware --NO_DEBUG --max_filesize=1000000000 --log_dir=logging

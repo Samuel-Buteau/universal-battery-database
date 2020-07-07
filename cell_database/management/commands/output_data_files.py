c3c72cffb266234da394ddc50c2628c3a4309ad4
Samuel Buteau <Samuel.Buteau@dal.ca>
2020-07-07 14:40:20 -0300

    defaults to no plot

--- cell_database/management/commands/output_data_files.py
+++ cell_database/management/commands/output_data_files.py
@@ -8,13 +8,17 @@ def output_files(options):
         dataset_name, wet_names, filt_names, filt_colors, filt_pos = get_dataset_labels(dataset)
         output_dataset_to_csv(data, dataset_name, wet_names, filt_names, csv_format_default,
                               options['output_dir'])
-        output_dataset_to_plot(data, dataset_name, wet_names, filt_names, filt_colors, filt_pos,
-                               options['output_dir'])
+        if options['visuals']:
+            output_dataset_to_plot(data, dataset_name, wet_names, filt_names, filt_colors, filt_pos,
+                                   options['output_dir'])
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('--output_dir', default='')
 
+        parser.add_argument('--visuals', dest='visuals', action='store_true')
+        parser.add_argument('--no-visuals', dest='visuals', action='store_false')
+        parser.set_defaults(visuals=False)
 
     def handle(self, *args, **options):
         output_files(options)

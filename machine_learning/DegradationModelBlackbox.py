a2892ac44def36e11bfb3a789443c48ff2c3339c
Samuel-Buteau <30840933+Samuel-Buteau@users.noreply.github.com>
2020-07-14 12:17:06 -0300

    Update DegradationModelBlackbox.py

--- machine_learning/DegradationModelBlackbox.py
+++ machine_learning/DegradationModelBlackbox.py
@@ -77,9 +77,17 @@ class DegradationModel(Model):
         self.fourier_features = bool(options[Key.FOUR_FEAT])
 
         self.sigma = options[Key.FF_SIGMA]
+        self.sigma_cycle = 1.
+        self.sigma_voltage = 1.
+        self.sigma_current = 1.
         self.d, self.f = 3, 32
+        random_matrix =  np.random.normal(0, self.sigma, (self.d, self.f))
+        random_matrix[0,:] *= self.sigma_cycle
+        random_matrix[1,:] *= self.sigma_voltage
+        random_matrix[2,:] *= self.sigma_current
+        
         self.random_gaussian_matrix = 2 * np.pi * tf.constant(
-            np.random.normal(0, self.sigma, (self.d, self.f)),
+            random_matrix,
             dtype = tf.float32,
         )
 
